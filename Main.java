1fad3b2fc4a244c8dd6d87069765d8703021b692
kokon02 <127319252+kokon02@users.noreply.github.com>
2023-03-22 21:42:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+   
+     double a = 50;
+    
+    double b = 10;
+    double suma = a + b;
+    System.out.println("Suma wynosi " +suma); 
+    double roznica = a - b;
+    System.out.println("Roznica wynosi " +roznica);
+    double iloczyn = a * b;
+    System.out.println("Iloczyn wynosi " +iloczyn);
+    double iloraz = a / b;
+     System.out.println("Iloraz wynosi " +iloraz);
+          
+          
+    
+        
+    
+  }
+}
